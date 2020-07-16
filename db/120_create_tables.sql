9de578c1348a5f6e39e094184aa5dbbe94752627
PhilCAEUser <rbyETE4Ceu2iA4Z>
2020-07-16 08:00:44 +0000

    Test

---
+++ db/120_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  caeschema
+-- Automatically generated sql script for the service 120, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table x.
+--
+CREATE TABLE caeschema.x (
+  y INTEGER ,
+CONSTRAINT y_PK PRIMARY KEY (y)
+ 
+);
+
+
+
